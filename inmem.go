428ac04c8bfebb5aee58b3cdc57be27ed2d19d1d
zaidfadhil <11668528+zaidfadhil@users.noreply.github.com>
2023-04-11 17:58:29 +0300

    fix inmem backend memory leak

--- inmem.go
+++ inmem.go
@@ -5,19 +5,32 @@ import (
 )
 
 type inMemoryBackend struct {
-	sync.Mutex
+	sync.RWMutex
 	tasks []*Task
+
+	head  int
+	tail  int
+	count int
 }
 
 func NewInMemoryBackend() *inMemoryBackend {
-	return &inMemoryBackend{}
+	return &inMemoryBackend{
+		tasks: make([]*Task, 1),
+	}
 }
 
 func (b *inMemoryBackend) Push(task *Task) error {
 	b.Lock()
 	defer b.Unlock()
 
-	b.tasks = append(b.tasks, task)
+	if b.count == len(b.tasks) {
+		b.resize(b.count * 2)
+	}
+
+	b.tasks[b.tail] = task
+	b.tail = (b.tail + 1) % len(b.tasks)
+	b.count++
+
 	return nil
 }
 
@@ -29,12 +42,39 @@ func (b *inMemoryBackend) Request() (*Task, error) {
 	b.Lock()
 	defer b.Unlock()
 
-	data := b.tasks[:1][0]
-	b.tasks = b.tasks[1:]
+	data := b.tasks[b.head]
+	b.tasks[b.head] = nil
+	b.head = (b.head + 1) % len(b.tasks)
+	b.count--
+
+	if n := len(b.tasks) / 2; n > 1 && b.count <= n {
+		b.resize(n)
+	}
 
 	return data, nil
 }
 
 func (b *inMemoryBackend) Close() error {
 	return nil
+}
+
+func (b *inMemoryBackend) Size() int {
+	b.Lock()
+	defer b.Unlock()
+	return len(b.tasks)
+}
+
+func (b *inMemoryBackend) resize(size int) {
+	nodes := make([]*Task, size)
+
+	if b.head < b.tail {
+		copy(nodes, b.tasks[b.head:b.tail])
+	} else {
+		copy(nodes, b.tasks[b.head:])
+		copy(nodes[len(b.tasks)-b.head:], b.tasks[:b.tail])
+	}
+
+	b.tail = b.count % size
+	b.head = 0
+	b.tasks = nodes
 }
